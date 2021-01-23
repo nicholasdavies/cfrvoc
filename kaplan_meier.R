777a0bf88b46a3bb726bf4b1bb98dd60730cc680
C Jarvis <Christopher1.jarvis@gmail.com>
2021-01-23 00:31:57 +0000

    Add an inset plot

--- kaplan_meier.R
+++ kaplan_meier.R
@@ -13,7 +13,7 @@ source("./hazard_data.R")
 theme_set(theme_cowplot(font_size = 10))
 
 # Load complete data set
-cd = complete_data("20210121")
+cd = complete_data("20210122")
 
 # Assemble data set
 dataS = model_data(cd, criterion = "under30CT", remove_duplicates = TRUE, death_cutoff = 28, reg_cutoff = 10, P_voc = 0, prevalence_cutoff = TRUE)
@@ -66,16 +66,42 @@ ggsave("./output/kmcurves.png", pl, width = 20, height = 30, units = "cm")
 km = survfit(Surv(time, status) ~ sgtf_label, data = dataS)
 plAA = KMunicate2(fit = km, time_scale = seq(0, 28, by = 4), .ylim = c(0.995, 1), .margin = 0.3, .title = "Overall", .legend_position = c(0.05, 0.1))
 
+plAA_inset = KMunicate2(fit = km, time_scale = seq(0, 28, by = 28), .ylim = c(0, 1), .margin = 0.3, .title = "", .risk_table = NULL, .legend_position = "none",.ylab = "", .xlab = "") + scale_y_continuous(breaks = c(0,1), expand = expansion(0)) 
+
+plAA_both <- 
+    ggdraw() +
+    draw_plot(plAA) +
+    draw_plot(plAA_inset, x = 0.7, y = .75, width = .25, height = .2)
+
 km = survfit(Surv(time, status) ~ sgtf_label, data = dataS[age < 70])
 plBB = KMunicate2(fit = km, time_scale = seq(0, 28, by = 4), .ylim = c(0.9985, 1), .margin = 0.3, .title = "Under 70", .legend_position = c(0.05, 0.1))
+
+plBB_inset = KMunicate2(fit = km, time_scale = seq(0, 28, by = 28), .ylim = c(0, 1), .margin = 0.3, .title = "", .risk_table = NULL, .legend_position = "none",.ylab = "", .xlab = "") + scale_y_continuous(breaks = c(0,1), expand = expansion(0)) 
+
+
+plBB_both <- 
+    ggdraw() +
+    draw_plot(plBB) +
+    draw_plot(plBB_inset, x = 0.7, y = .75, width = .25, height = .2)
+
 
 km = survfit(Surv(time, status) ~ sgtf_label, data = dataS[age >= 70])
 plCC = KMunicate2(fit = km, time_scale = seq(0, 28, by = 4), .ylim = c(0.92, 1), .margin = 0.3, .title = "70 or older", .legend_position = c(0.05, 0.1))
 
+plCC_inset = KMunicate2(fit = km, time_scale = seq(0, 28, by = 28), .ylim = c(0, 1), .margin = 0.3, .title = "", .risk_table = NULL, .legend_position = "none",.ylab = "", .xlab = "") + scale_y_continuous(breaks = c(0,1), expand = expansion(0)) 
+
+plCC_both <- 
+    ggdraw() +
+    draw_plot(plCC) +
+    draw_plot(plCC_inset, x = 0.7, y = .75, width = .25, height = .2)
 
 pl = cowplot::plot_grid(plAA, plBB, plCC, nrow = 1, labels = letters, label_size = 10)
 ggsave("./output/kmcurves2.pdf", pl, width = 45, height = 15, units = "cm", useDingbats = FALSE)
 ggsave("./output/kmcurves2.png", pl, width = 45, height = 15, units = "cm")
+
+pl_inset = cowplot::plot_grid(plAA_both, plBB_both, plCC_both, nrow = 1, labels = letters, label_size = 10)
+ggsave("./output/kmcurves2_inset.pdf", pl_both, width = 45, height = 15, units = "cm", useDingbats = FALSE)
+ggsave("./output/kmcurves2_inset.png", pl_both, width = 45, height = 15, units = "cm")
 
 
 
